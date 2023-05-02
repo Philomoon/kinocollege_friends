7b20b98f5ac32839a059a92c522cc788f28ec2e4
Philomoon <likeamoon0712@gmail.com>
2023-05-02 21:05:41 +0900

    問１−１３　まで完了

--- web_app10/accounts_app/urls.py
+++ web_app10/accounts_app/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     # accounts_signup viewへのアクセスを追加しましょう。
     path('signup/', views.accounts_signup, name='signup'),
     # 問 1-13-1 accounts_signup 関数へのパスを追加しましょう。url は signup_admin/、name は signup_admin としましょう。
-
+    path('signup_admin/',views.accounts_signup,name='signup_admin'),
 ]
